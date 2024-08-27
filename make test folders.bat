75745b1a869ccfaa3f45f2f00ab1f0137600ac5e
Major-Okamino <inanahsan-15-2019413037@mis.du.ac.bd>
2024-08-27 15:05:38 +0600

    Add files via upload
    uploaded .bat script file from PC

---
+++ make test folders.bat
@@ -0,0 +1,32 @@
+@echo off
+
+
+md "A/A1","A/A2","A/A3","A/A4","A/A5","B/B1","B/B2","B/B3","B/B4","B/B5","C/C1","C/C2","C/C3","C/C4","C/C5","D/D1","D/D2","D/D3","D/D4","D/D5","E/E1","E/E2","E/E3","E/E4","E/E5"
+
+
+echo > "A/A1/JDOperations Engineer.docx",
+echo > "A/A2/Job_DescriptionNetworking Expert.docx",
+echo > "A/A3/Job_descMachine Learning Associate.docx",
+echo > "A/A4/J_DBrand Manager.docx",
+echo > "A/A5/Job DescriptionResearch and Development.docx",
+echo > "B/B1/JD_Chief of Security.docx",
+echo > "B/B2/JD Head of Operations.docx",
+echo > "B/B3/JDAsset Management.docx",
+echo > "B/B4/Job_DescriptionSupply Chain Analyst.docx",
+echo > "B/B5/Job_descData Analyst .docx",
+echo > "C/C1/J_DData Engineer.docx",
+echo > "C/C2/Job DescriptionSystems Manager.docx",
+echo > "C/C3/JD_Brand Strategist.docx",
+echo > "C/C4/JD Innovation and Change Manager.docx",
+echo > "C/C5/JDHead of Audit.docx",
+echo > "D/D1/Job_DescriptionInternal Auditor.docx",
+echo > "D/D2/Job_descAudit Manager.docx",
+echo > "D/D3/J_DStaff Accountant .docx",
+echo > "D/D4/Job DescriptionFinancial Analyst.docx",
+echo > "D/D5/JD_Sourcing Manager.docx",
+echo > "E/E1/JD Office Administrator .docx",
+echo > "E/E2/JDReceptionist.docx",
+echo > "E/E3/Job_DescriptionJanitor.docx",
+echo > "E/E4/Job_descOffice Assistant.docx",
+echo > "E/E5/J_DJunior Consultant .docx"
+
